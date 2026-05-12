d6f93c99ba130c552c5480eb731bcbabbfeb1342
Nikolay Matskevich <kolyan_krytoj@mail.ru>
2026-05-12 15:13:46 +0700

    Added 4 main func for BV

--- main.c
+++ main.c
@@ -1,18 +1,116 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
-void PrintBV (unsigned int vec)
+unsigned char * logMul(unsigned char *vecA, size_t bitsA, unsigned char *vecB, size_t bitsB);
+unsigned char * logSum(unsigned char *vecA, size_t bitsA, unsigned char *vecB, size_t bitsB);
+unsigned char * sumMod2(unsigned char *vecA, size_t bitsA, unsigned char *vecB, size_t bitsB);
+void inversion(unsigned char *vec, size_t bits);
+void shiftLeft(unsigned char *vec, size_t bits, size_t k);
+void shiftRight(unsigned char *vec, size_t bits, size_t k);
+void set1(unsigned char *vec, size_t bits, size_t bit);
+void set0(unsigned char *vec, size_t bits, size_t bit);
+
+int main()
 {
-    unsigned int  mask = 1;
-    mask = mask<<31;
+    return 0;
+}
 
-    for(int ix=0; ix < 32; ix++)
+unsigned char * logMul(unsigned char *vecA, size_t bitsA, unsigned char *vecB, size_t bitsB)
+{
+    unsigned char* res = NULL;
+    if (vecA && vecB && bitsA && bitsB && bitsA == bitsB)
     {
-        if ( mask & vec) printf("%c", '1');
-        else printf("%c", '0');
-        mask = mask>>1;
+        size_t bytes = ((bitsA - 1) / 8) + 1;
+        res = (unsigned char *)calloc(bytes, sizeof(unsigned char));
+        if (res)
+        {
+            for (size_t i = 0; i < bytes; i++)
+            {
+                res[i] = vecA[i] & vecB[i];
+            }
+        }
     }
-    printf("\n");
+    return res;
+}
+
+unsigned char * logSum(unsigned char *vecA, size_t bitsA, unsigned char *vecB, size_t bitsB)
+{
+    unsigned char* res = NULL;
+    if (vecA && vecB && bitsA && bitsB && bitsA == bitsB)
+    {
+        size_t bytes = ((bitsA - 1) / 8) + 1;
+        res = (unsigned char*)calloc(bytes, sizeof (unsigned char));
+        if (res)
+        {
+            for (size_t i = 0; i < bytes; i++)
+            {
+                res[i] = vecA[i] | vecB[i];
+            }
+        }
+    }
+    return res;
+}
+
+unsigned char * sumMod2(unsigned char *vecA, size_t bitsA, unsigned char *vecB, size_t bitsB)
+{
+    unsigned char* res = NULL;
+    if (vecA && vecB && bitsA && bitsB && bitsA == bitsB)
+    {
+        size_t bytes = ((bitsA - 1) / 8) + 1;
+        res = (unsigned char*)calloc(bytes, sizeof (unsigned char));
+        if (res)
+        {
+            for (size_t i = 0; i < bytes; i++)
+            {
+                res[i] = vecA[i] ^ vecB[i];
+            }
+        }
+    }
+    return res;
+}
+
+void inversion(unsigned char *vec, size_t bits)
+{
+    if (vec && bits)
+    {
+        size_t bytes = ((bits - 1) / 8) + 1;
+        for (size_t i = 0; i < bytes; i++)
+        {
+            vec[i] = ~vec[i];
+        }
+    }
+}
+
+void printLongBv(unsigned char* vec, size_t bits)
+{
+    if (vec)
+    {
+        if (bits)
+        {
+            printf("%s", vec);
+        }
+        else
+        {
+            printf("Null length BV recieved! \n");
+        }
+    }
+    else
+    {
+        printf("Null address recieved! \n");
+    }
+}
+
+void shiftLeft(unsigned char *vec, size_t bits, size_t k)
+{
+
+}
+
+
+void PrintBV (unsigned char* label, unsigned char* vec, size_t bits)
+{
+
 }
 
 unsigned int StrToInt(char* vec)
@@ -35,9 +133,4 @@ unsigned int StrToInt(char* vec)
     return res;
 }
 
-int main()
-{
-    unsigned int vecA = StrToInt("10101010");
-    PrintBV(vecA);
-    return 0;
-}
+
